d6618453a931b593a6262c56febf9c23dcbf112b
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2021-07-28 22:37:56 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Hola que hace")
+const checkbox = document.querySelector('#checkbox')
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    checkbox.setAttribute('checked', true)
+}
+
+checkbox.addEventListener('change', function(event) {
+    console.log(this.checked)
+    if (this.checked) {
+        document.body.classList.add('is-dark-mode')
+    } else {
+        document.body.classList.remove('is-dark-mode')
+    }
+})
